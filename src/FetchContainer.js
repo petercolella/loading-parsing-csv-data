607806ff395bcd1cdddf11d30720597c026dfd06
petercolella <peter.joseph.colella@gmail.com>
2021-06-15 11:25:20 -0400

    adds d3 to parse csv text and display details

--- src/FetchContainer.js
+++ src/FetchContainer.js
@@ -3,6 +3,12 @@ import React, { useEffect, useState } from "react";
 const csvUrl =
   "https://gist.githubusercontent.com/curran/b236990081a24761f7000567094914e0/raw/cssNamedColors.csv";
 
+const styles = {
+  pre: {
+    fontSize: "2rem",
+  },
+};
+
 const FetchContainer = () => {
   const [data, setData] = useState("");
 
@@ -13,9 +19,13 @@ const FetchContainer = () => {
     const fetchTextAsync = async (url) => {
       try {
         const response = await fetch(url, { signal });
-        const data = await response.text();
+        const text = await response.text();
         if (!signal.aborted) {
-          setData(data);
+          const data = d3.csvParse(text);
+          let message = `${(text.length / 1024).toFixed(2)} kB
+${data.length} rows
+${data.columns.length} columns`;
+          setData(message);
         }
       } catch (err) {
         console.log(err);
@@ -31,8 +41,8 @@ const FetchContainer = () => {
 
   return (
     <div style={{ margin: 20 }}>
-      <h1>Our data will be below:</h1>
-      <p>{data ? data : "Waiting for data..."}</p>
+      <h1>Our data details:</h1>
+      <pre style={styles.pre}>{data ? data : "Waiting for data..."}</pre>
     </div>
   );
 };
